ebddafa5f03f06efba6bb4937075501c2bdff94c
Kabeer Ali <100375085+kabeerali1825@users.noreply.github.com>
2025-07-09 22:43:16 +0500

    Added wpm.ClinicApi

---
+++ src/Wpm.Clinic.Api/Application/ClinicApplicationService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Wpm.Clinic.Controllers;
+using Wpm.Clinic.DataAccess;
+using Wpm.Clinic.ExternalServices;
+
+namespace Wpm.Clinic.Application;
+
+public class ClinicApplicationService(ClinicDbContext dbContext,
+    ManagementService managementService, IMemoryCache memoryCache)
+{
+    public async Task<Consultation> Handle(StartConsultationCommand command)
+    {
+        PetInfo? petInfo = await memoryCache.GetOrCreateAsync(command.PatientId,
+            async cacheEntry =>
+            {
+                cacheEntry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(30);
+                return await managementService.GetPetInfo(command.PatientId);
+            });
+
+        var newConsultation = new Consultation(Guid.NewGuid(),
+                                               command.PatientId,
+                                               petInfo.Name,
+                                               petInfo.Age,
+                                               DateTime.UtcNow);
+        await dbContext.Consultations.AddAsync(newConsultation);
+        await dbContext.SaveChangesAsync();
+
+        return newConsultation;
+    }
+}
---
+++ src/Wpm.Clinic.Api/Controllers/ConsultationController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Wpm.Clinic.Application;
+
+namespace Wpm.Clinic.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ConsultationController(ClinicApplicationService applicationService) : ControllerBase
+{
+    [HttpPost("start")]
+    public async Task<IActionResult> Start(StartConsultationCommand command)
+    {
+        var result = await applicationService.Handle(command);
+        return Ok(result);
+    }
+}
+
+public record StartConsultationCommand(int PatientId);
